bb642e39860d4bb8d3030d129792023c1ec6d235
ocawebtech <ocawebtech@gmail.com>
2024-03-17 13:40:48 +0100

    added spinner

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
+let uploadResultDiv = document.getElementById("uploadResult");
 document
   .getElementById("uploadForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
+    document.querySelector(".loader").style.display = "block";
 
-    var formData = new FormData(this);
+    const formData = new FormData(this);
     fetch("/upload", {
       method: "POST",
       body: formData,
@@ -11,10 +13,12 @@ document
       .then((response) => response.json())
       .then((data) => {
         // Display the URL of the uploaded web app
-        var uploadResultDiv = document.getElementById("uploadResult");
+        // Hide spinner after upload is complete
+        document.querySelector(".loader").style.display = "none";
         uploadResultDiv.innerHTML = data.link;
       })
       .catch((error) => {
+        document.querySelector(".loader").style.display = "none";
         console.error("Error:", error);
       });
   });
